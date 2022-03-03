1cfd31b17db3e668ed616ce7f90a0cf48b039cd3
Cristian Socha <ccsocha04@gmail.com>
2022-03-03 00:05:17 -0500

    1. Geting version GDB - 2. Extracting files

--- Validador/validator_web.py
+++ Validador/validator_web.py
@@ -9,14 +9,14 @@ def get_version_datasets(version: str) -> List[str]:
     """
     Get all feature datasets in a geodatabase
     """
-    arcpy.AddMessage("Getting version datasets...")
+    arcpy.AddMessage("1. Getting version ...")
     walk_gvds = arcpy.da.Walk(topdown=True, datatype="FeatureDataset")
     feature_datasets = []
     for dirpath, dirnames, fnames in walk_gvds:
         for gvds in dirnames:
             feature_datasets.append(gvds)
     
-    print("Version_Datasets: ",len(feature_datasets))
+    arcpy.AddMessage(F"1.1 Version_Datasets: {len(feature_datasets)}")
     
     return feature_datasets
 
@@ -24,14 +24,13 @@ def get_version_feature_classes(version: str) -> List[str]:
     """
     Get all feature classes in a geodatabase
     """
-    arcpy.AddMessage("Getting version feature classes...")
     walk_gvfcs = arcpy.da.Walk(topdown=True, datatype="FeatureClass")
     feature_classes = []
     for dirpath, dirnames, fnames in walk_gvfcs:
         for fc in fnames:
             feature_classes.append(fc)
     
-    print("Version_Feature_Classes: ",len(feature_classes))
+    arcpy.AddMessage(F"1.2 Version_Feature_Classes: {len(feature_classes)}")
     
     return feature_classes
 
@@ -39,14 +38,13 @@ def get_version_tables(version: str) -> List[str]:
     """
     Get all tables in a geodatabase
     """
-    arcpy.AddMessage("Getting version tables...")
     walk_gvtables = arcpy.da.Walk(topdown=True, datatype="Table")
     tables = []
     for dirpath, dirnames, fnames in walk_gvtables:
         for table in fnames:
             tables.append(table)
     
-    print("Version_Tables: ",len(tables))
+    arcpy.AddMessage(F"1.3 Version_Tables: {len(tables)}")
     
     return tables
 
@@ -54,24 +52,35 @@ def extract_files(file_path, extract_path):
     """
     Extract all files from a zip file
     """
-    arcpy.AddMessage("Extracting files...")
-    
+    arcpy.AddMessage("2. Extracting files...")
     with ZipFile(file_path, "r") as zip:
-        
         file_name = os.path.basename(zip.filename).split(".")[0]
         file_format = os.path.basename(zip.filename).split(".")[1]
         file_date = file_name.split("_")[1]
 
-        arcpy.AddMessage("File name: {}".format(file_name))
-        arcpy.AddMessage("File format: {}".format(file_format))
-        arcpy.AddMessage("File date: {}".format(file_date))
+        zip.extractall(path=extract_path)
 
-        list_files = zip.namelist()
+        if file_format != "gdb":
+            arcpy.AddWarning("File format is not gdb")    
+            list_files = zip.namelist()
+            for file in list_files:
+                arcpy.AddMessage(F"2.1 File name: {file.split('.')[0]}")
+                arcpy.AddMessage(F"2.2 File format: {file.split('.')[1]}")
+                arcpy.AddMessage(F"2.3 File date: {file_date}")
+            conversion_format()
+        else:
+            arcpy.AddMessage(F"2.1 File name: {file_name}")
+            arcpy.AddMessage(F"2.2 File format: {file_format}")
+            arcpy.AddMessage(F"2.3 File date: {file_date}")
 
-        for file in list_files:
-            print("Extracting file: {}".format(file))
-
-        # zip.extractall(path=extract_path)
+def conversion_format():
+    """
+    Convert all files to a geodatabase
+    """
+    arcpy.AddMessage("2.4 Converting files to geodatabase...")
+    arcpy.AddMessage("2.5 Converting feature datasets...")
+    arcpy.AddMessage("2.6 Converting feature classes...")
+    arcpy.AddMessage("2.7 Converting tables...")
 
 def get_feature_datasets():
     """
@@ -85,8 +94,8 @@ def get_feature_datasets():
             feature_datasets.append(fd)
             fds_crs = arcpy.Describe(os.path.join(dirpath, fd)).spatialReference.GCSCode
     
-    print("Feature_DataSets: ",len(feature_datasets))
-    print(feature_datasets)
+    arcpy.AddMessage(F"Feature_DataSets: {len(feature_datasets)}")
+    arcpy.AddMessage(feature_datasets)
     
     get_feature_classes()
     
@@ -105,8 +114,8 @@ def get_feature_classes():
             feature_classes.append(fc)
             fc_crs = arcpy.Describe(os.path.join(dirpath, fc)).spatialReference.GCSCode
     
-    print("Feature_Classes: ",len(feature_classes))
-    print(feature_classes)
+    arcpy.AddMessage(F"Feature_Classes: {len(feature_classes)}")
+    arcpy.AddMessage(feature_classes)
 
     get_tables()
 
@@ -124,8 +133,8 @@ def get_tables():
         for table in fnames:
             tables.append(table)
     
-    print("Tables: ",len(tables))
-    print(tables)
+    arcpy.AddMessage(F"Tables: {len(tables)}")
+    arcpy.AddMessage(tables)
     
     return tables
 
@@ -142,9 +151,9 @@ if __name__ == '__main__':
     # Tables
     version_tables = get_version_tables(path)
     
-    # Inputs
-    file_path = r"C:\UTGI\SoftwareEstrategico\ANNA\Python\Validador_Web\Data\0-178_202200301.gdb.zip"
-    # file_path = r"C:\UTGI\SoftwareEstrategico\ANNA\Python\Validador_Web\Data\0-179_202200301.zip"
+    # Get input GDB / GPKG    
+    # file_path = r"C:\UTGI\SoftwareEstrategico\ANNA\Python\Validador_Web\Data\0-178_202200301.gdb.zip"
+    file_path = r"C:\UTGI\SoftwareEstrategico\ANNA\Python\Validador_Web\Data\0-179_202200301.zip"
     extract_path = r"C:\UTGI\SoftwareEstrategico\ANNA\Python\Validador_Web\Data"
 
     # Set the workspace
@@ -156,5 +165,6 @@ if __name__ == '__main__':
     """
 
     # Extract files
-    # extract_files(file_path, extract_path)
+    extract_files(file_path, extract_path)
+
     #fds = get_feature_datasets()
