46e0894b1c7a3072aef27a0bcfb53d208c239b5f
Cristian Socha <ccsocha04@gmail.com>
2022-03-03 23:15:46 -0500

    4. Spatial matching

--- Validador/validator_web.py
+++ Validador/validator_web.py
@@ -1,5 +1,6 @@
 from importlib.resources import path
 import os
+import sys
 from typing import List
 import arcpy
 from zipfile import ZipFile
@@ -93,6 +94,199 @@ def set_workspace(path: str):
     """
     arcpy.AddMessage("3. Setting workspace...")
     arcpy.env.workspace = path
+    arcpy.env.overwriteOutput = True
+
+def spatial_matching():
+    """
+    Spatial matching
+    """
+    arcpy.AddMessage("4. Spatial matching...")
+    
+    # TODO: request WFS
+    geojson_servicioANM_1 = {
+        "displayFieldName": "",
+        "fieldAliases": {
+            "OBJECTID": "OBJECTID",
+            "SHAPE_Length": "SHAPE_Length",
+            "SHAPE_Area": "SHAPE_Area"
+        },
+        "geometryType": "esriGeometryPolygon",
+        "spatialReference": {
+            "wkid": 4686,
+            "latestWkid": 4686
+        },
+        "fields": [
+            {
+                "name": "OBJECTID",
+                "type": "esriFieldTypeOID",
+                "alias": "OBJECTID"
+            },
+            {
+                "name": "SHAPE_Length",
+                "type": "esriFieldTypeDouble",
+                "alias": "SHAPE_Length"
+            },
+            {
+                "name": "SHAPE_Area",
+                "type": "esriFieldTypeDouble",
+                "alias": "SHAPE_Area"
+            }
+        ],
+        "features": [
+            {
+                "geometry": {
+                    "rings": [
+                        [
+                            [
+                                -74.47880612799997,
+                                5.42205679500006
+                            ],
+                            [
+                                -74.49103233099999,
+                                5.422048681000035
+                            ],
+                            [
+                                -74.49105460199996,
+                                5.455144681000036
+                            ],
+                            [
+                                -74.47882773099997,
+                                5.455152845000043
+                            ],
+                            [
+                                -74.47880612799997,
+                                5.42205679500006
+                            ]
+                        ]
+                    ]
+                },
+                "attributes": {
+                    "CODIGO_EXPEDIENTE": "IDO-08061",
+                    "AREA_HA": 495.92897,
+                    "FECHA_DE_INSCRIPCION": "8/06/2010",
+                    "ESTADO": "Activo",
+                    "MODALIDAD": "CONTRATO DE CONCESION (L 685)",
+                    "ETAPA": "Explotación",
+                    "MINERALES": "ANTRACITA, CARBÓN METALÚRGICO, CARBÓN TÉRMICO",
+                    "NOMBRE_DE_TITULAR": "INVERSIONES NUEVA COLONIA S.A.S.",
+                    "NUMERO_IDENTIFICACION": "900866306",
+                    "TIPO_DE_IDENTIFICACION": "NIT",
+                    "IDENTIFICACION_TITULARES": "57086",
+                    "PTO_PTI": "PTO",
+                    "INSTRUMENTO_AMBIENTAL": "Y",
+                    "DEPARTAMENTOS": "Cundinamarca",
+                    "MUNICIPIOS": "CAPARRAPÍ",
+                    "GRUPO_DE_TRABAJO": "ZONA CENTRO",
+                    "FECHA_TERMINACION": "7/06/2040",
+                    "OBJECTID": 5178,
+                    "SHAPE_Length": 9.06451439618818E-02,
+                    "SHAPE_Area": 4.04649952760689E-04
+                }
+            }
+        ]
+    }
+    geojson_servicioANM_2 = {
+        "displayFieldName": "",
+        "fieldAliases": {
+            "OBJECTID": "OBJECTID",
+            "SHAPE_Length": "SHAPE_Length",
+            "SHAPE_Area": "SHAPE_Area"
+        },
+        "geometryType": "esriGeometryPolygon",
+        "spatialReference": {
+            "wkid": 4686,
+            "latestWkid": 4686
+        },
+        "fields": [
+            {
+                "name": "OBJECTID",
+                "type": "esriFieldTypeOID",
+                "alias": "OBJECTID"
+            },
+            {
+                "name": "SHAPE_Length",
+                "type": "esriFieldTypeDouble",
+                "alias": "SHAPE_Length"
+            },
+            {
+                "name": "SHAPE_Area",
+                "type": "esriFieldTypeDouble",
+                "alias": "SHAPE_Area"
+            }
+        ],
+        "features": [
+            {
+                "geometry": {
+                    "rings": [
+                        [
+                            [
+                                -74.37196390799994,
+                                5.441125020000072
+                            ],
+                            [
+                                -74.37433568799997,
+                                5.438257351000061
+                            ],
+                            [
+                                -74.37716077099998,
+                                5.439847478000047
+                            ],
+                            [
+                                -74.39108764699995,
+                                5.429558979000035
+                            ],
+                            [
+                                -74.39307373099996,
+                                5.431465956000068
+                            ],
+                            [
+                                -74.37623310399994,
+                                5.443410792000066
+                            ],
+                            [
+                                -74.37196390799994,
+                                5.441125020000072
+                            ]
+                        ]
+                    ]
+                },
+                "attributes": {
+                    "CODIGO_EXPEDIENTE": "GBH-141",
+                    "AREA_HA": 76.67445,
+                    "FECHA_DE_INSCRIPCION": "7/09/2006 3:38:11 a.m.",
+                    "ESTADO": "Titulo terminado-en proceso de liquidacion",
+                    "MODALIDAD": "CONTRATO DE CONCESION (L 685)",
+                    "ETAPA": "Explotación",
+                    "MINERALES": "ANHIDRITA, ANTRACITA, ARCILLA COMUN, ARCILLAS, ARCILLAS ESPECIALES, ARCILLAS REFRACTARIAS, ARENAS, ARENAS ARCILLOSAS, ARENAS FELDESPÁTICAS, ARENAS INDUSTRIALES, ARENAS Y GRAVAS SILICEAS, ARENISCAS, ASFALTO NATURAL, AZUFRE, BAUXITA, BENTONITA, CALCITA, CAOLIN, CARBÓN, CARBÓN METALÚRGICO, CARBÓN TÉRMICO, CONCENTRADOS MINERALES DE IRIDIO, CORINDON, CUARZO, DOLOMITA, ESMERALDA, FELDESPATOS, FLUORITA, GRAFITO, GRANATE, GRANITO, GRAVAS, MAGNESITA, MARMOL Y TRAVERTINO, MICA, MINERALES DE ALUMINIO Y SUS CONCENTRADOS, MINERALES DE ANTIMONIO Y SUS CONCENTRADOS, MINERALES DE BARIO, MINERALES DE BORO, MINERALES DE CIRCONIO Y SUS CONCENTRADOS, MINERALES DE COBALTO Y SUS CONCENTRADOS, MINERALES DE COBRE Y SUS CONCENTRADOS, MINERALES DE CROMO Y SUS CONCENTRADOS, MINERALES DE ESTAÑO Y SUS CONCENTRADOS, MINERALES DE HIERRO Y SUS CONCENTRADOS, MINERALES DE LITIO , MINERALES DE MANGANESO Y SUS CONCENTRADOS, MINERALES DE MERCURIO Y SUS CONCENTRADOS, MINERALES DE MOLIBDENO Y SUS CONCENTRADOS, MINERALES DE NIQUEL Y SUS CONCENTRADOS, MINERALES DE ORO Y SUS CONCENTRADOS, MINERALES DE PLATA Y SUS CONCENTRADOS, MINERALES DE PLATINO (INCLUYE PLATINO, PALADIO, RUTENIO, RODIO, OSMIO) Y SUS CONCENTRADOS, MINERALES DE PLOMO Y SUS CONCENTRADOS, MINERALES DE POTASIO, MINERALES DE SODIO, MINERALES DE TANTALIO, MINERALES DE TIERRAS RARAS, MINERALES DE TITANIO Y SUS CONCENTRADOS, MINERALES DE VANADIO Y SUS CONCENTRADOS, MINERALES DE WOLFRAMIO (TUNGSTENO) Y SUS CONCENTRADOS, MINERALES DE ZINC Y SUS CONCENTRADOS, MINERALES Y CONCENTRADOS DE TORIO, MINERALES Y CONCENTRADOS DE URANIO, OTRAS PIEDRAS PRECIOSAS, OTRAS PIEDRAS SEMIPRECIOSAS, OTRAS ROCAS METAMÓRFICAS, OTRAS ROCAS Y MINERALES DE ORIGEN VOLCANICO, OTROS MINERALES DE ALUMINIO Y SUS CONCENTRADOS, PIEDRA POMEZ, PIRITA, PIZARRA, RECEBO, ROCA FOSFATICA, ROCA O PIEDRA CALIZA, ROCA O PIEDRA CORALINA, ROCAS DE CUARCITA, ROCAS DE ORIGEN VOLCÁNICO, PUZOLANA, BASALTO, SAL GEMA, SAL MARINA, SULFATO DE BARIO NATURAL-BARITINA, TALCO, YESO",
+                    "NOMBRE_DE_TITULAR": "RAFAEL GARCIA DELGADO, JOSE RAMON GUERRERO SANTAFE, ARMANDO RAMIREZ MARIN, JOSE FAUBRICIO ZAMUDIO ANZOLA, ANDRES BOTERO HERRERA",
+                    "NUMERO_IDENTIFICACION": "79047350, 10218624, 19341607, 3079240, 79147444",
+                    "TIPO_DE_IDENTIFICACION": "Cédula de Ciudadanía, Cédula de Ciudadanía, Cédula de Ciudadanía, Cédula de Ciudadanía, Cédula de Ciudadanía",
+                    "IDENTIFICACION_TITULARES": "24816, 27744, 35140, 39582, 41542",
+                    "PTO_PTI": "null",
+                    "INSTRUMENTO_AMBIENTAL": "N",
+                    "DEPARTAMENTOS": "Cundinamarca",
+                    "MUNICIPIOS": "YACOPÍ",
+                    "GRUPO_DE_TRABAJO": "ZONA CENTRO",
+                    "FECHA_TERMINACION": "8/07/2019",
+                    "OBJECTID": 3629,
+                    "SHAPE_Length": 5.25209810700847E-02,
+                    "SHAPE_Area": 6.25627113648606E-05
+                }
+            }
+        ]
+    }
+
+    mining_title = arcpy.AsShape(geojson_servicioANM_1, True)
+    delimit_proyect_pg = arcpy.MakeFeatureLayer_management(os.path.join(arcpy.env.workspace, "TOPOGRAFIA_LOCAL", "DELIMIT_PROYEC_PG"))
+    select_location = arcpy.SelectLayerByLocation_management(delimit_proyect_pg, "ARE_IDENTICAL_TO", mining_title, None, "NEW_SELECTION", "NOT_INVERT")
+
+    if int(arcpy.GetCount_management(select_location)[0]) > 0:
+        arcpy.AddMessage("Existe una zona de explotacion en el proyecto")
+        return True
+    else:
+        arcpy.AddMessage("No existe una zona de explotacion en el proyecto")
+        return False
+
 
 def get_feature_datasets():
     """
@@ -156,6 +350,7 @@ if __name__ == '__main__':
     # Get version GDB
     gdb_structure = r"C:\UTGI\SoftwareEstrategico\ANNA\Python\Validador_Web\Estructura\SIGM_MINERIA.gdb"
     path = arcpy.env.workspace = gdb_structure
+    arcpy.env.overwriteOutput = True
     # Feature Datasets
     version_ds = get_version_datasets(path)
     # Feature Classes
@@ -164,24 +359,39 @@ if __name__ == '__main__':
     version_tables = get_version_tables(path)
     
     # Get input GDB / GPKG    
-    file_path = r"C:\UTGI\SoftwareEstrategico\ANNA\Python\Validador_Web\Data\0-178_202200301.gdb.zip"
-    # file_path = r"C:\UTGI\SoftwareEstrategico\ANNA\Python\Validador_Web\Data\0-179_202200301.zip"
+    # file_path = r"C:\UTGI\SoftwareEstrategico\ANNA\Python\Validador_Web\Data\GBH-141_20220303.gdb.zip"
+    file_path = r"C:\UTGI\SoftwareEstrategico\ANNA\Python\Validador_Web\Data\IDO-08061_20220303.gdb.zip"
+    # file_path = r"C:\UTGI\SoftwareEstrategico\ANNA\Python\Validador_Web\Data\IDO-08062_20220303.zip"
     extract_path = r"C:\UTGI\SoftwareEstrategico\ANNA\Python\Validador_Web\Data"
 
     # Extract files
     extract_files(file_path, extract_path)
+    
+    # Validator 1 - Spatial Matching
+    validate_1 = spatial_matching()
+    print(validate_1)
 
-    # Validate 1
-    #fds = get_feature_datasets()
+    """
+    File code -> Form (Front-end) // 0-178 
+    
+    AsShape
+    file_code = "IDO-08061"
+    file_code = "GBH-141"
+    type_name = "Titulo_Vigente"
 
-    # Validate 2
-    #fds = get_feature_datasets()
+    """
 
-    # Validate 3
-    #fds = get_feature_datasets()
+    
 
-    # Validate 4
-    #fds = get_feature_datasets()
+    # compare_result = arcpy.management.FeatureCompare(delimit_proyect_pg, mining_title, "OBJECTID", "GEOMETRY_ONLY", None, "0.000000008983 DecimalDegrees", 0.00, 0.00, None, None, "NO_CONTINUE_COMPARE", None)
+    # print(compare_result[1])
 
-    # Validate 5
+
+
+    # arcpy.MakeFeatureLayer_management(os.path.join(arcpy.env.workspace, "TOPOGRAFIA_LOCAL", "DELIMIT_PROYEC_PG"), "delimit_proyect_pg")
+
+    # arcpy.FeatureCompare_management(delimit_proyect_pg, mining_title, compare_type=)
+
+    # Validator 2 - Feature Comparison
+    
     #fds = get_feature_datasets()
