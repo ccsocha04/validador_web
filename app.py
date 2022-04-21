0a41e8720360a112e9e53ae2dcfa7651faf439ed
Cristian Socha <ccsocha04@gmail.com>
2022-04-21 08:22:17 -0500

    Depuración código app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import arcpy
 import pandas as pd
 from pathlib import Path
 from runpy import run_path
@@ -14,22 +15,30 @@ from database.gdb_path_to_validate import por_validar, gdb_para_validar, update_
 
 
 if __name__ == '__main__':
-    # cantidad = por_validar(con)
-
-    #if cantidad > 0:
-    #path = r'E:\VWMDG\validador_web\Data\IDO-08061_202203071.gdb'
-    path = r'C:\UTGI\SoftwareEstrategico\ANNA\Python\Validador_Web\Data\IDO-08061_202203071.gdb'
     
-    expediente = 'IDO-08061'
-    documento_tecnico = 'Formato Básico Minero - FBM'
-    # etapa = 'Construcción y montaje'
-    etapa = 'Exploración'
+    path = arcpy.GetParameterAsText(0)
+    expediente = arcpy.GetParameterAsText(1)
+    documento_tecnico = arcpy.GetParameterAsText(2)
+    etapa = arcpy.GetParameterAsText(3)
     version='1'
+    	
+    # path = r"C:\UTGI\SoftwareEstrategico\ANNA\Python\Validate_Web_Repository\Extract\IDO-08061_202203071.gdb"
+    # path = r"C:\UTGI\SoftwareEstrategico\ANNA\Python\Validate_Web_Repository\Files\IDO-08061_202203071.gdb.zip"
+    # expediente = 'IDO-08061'
+    # documento_tecnico = 'Formato Básico Minero - FBM'
+    # etapa = 'Exploración'
+    # version='1'
+
+    extract_path = r"C:\UTGI\SoftwareEstrategico\ANNA\Python\Validate_Web_Repository\Extract"
+
+    arcpy.AddMessage('1. Database connection')
+
+    # if path.endswith('.zip'):
+    #     path_validate = path[:-4]
+
     id_bd_gdb, ruta_gdb = gdb_para_validar(engine, gdb=path)
-    # TODO UPDATE INICIO_VALIDACION
     update_estado(con, id=id_bd_gdb, estado='En proceso')
     borrar_registros_mensajes(con, id=id_bd_gdb)
-
 
     # Get version Feature Datasets
     version_ds = get_version_datasets(engine)
@@ -42,16 +51,13 @@ if __name__ == '__main__':
     # Get versionFeatures attributes
     version_att = get_version_attributes(engine)
 
-    
-    # file_path = str(current_path.parent.absolute().joinpath(data_folder).joinpath(zip_file))
-    # TODO esto debe ser leido de la base de datos
-    # extract_path = str(current_path.parent.absolute().joinpath(data_folder))
-
     # Extract files
     # TODO verificar que path no termine en zip. si es así se debe desempaquetar y actualizar la ruta
     #  enla base de datos
-    # if ruta_gdb.endswith('.zip'):
-    #    extract_files(file_path, extract_path)
+    # TODO esto debe ser leido de la base de datos
+    extract_files(path, extract_path)
+
+    # Set Workspace
     set_workspace(ruta_gdb)
 
     # Get Feature Datasets
@@ -63,26 +69,18 @@ if __name__ == '__main__':
     # Get Feature Attributes
     attributes = get_feature_attributes(version_fc, fc)
 
-
-
     # Validator 1 - Spatial Matching
     spatial_matching(con, id_bd_gdb, expediente)
-
     # Validator 2 - Reference System
     df_reference = reference_system(version, engine, id_bd_gdb, version_ds, ds)
-
     # Validator 3 - Quantity of datasets
     df_datasets = quantity_dataset(engine, id_bd_gdb, version_ds, ds)
-
     # Validator 4 - Quantity of feature classes
     df_features = quantity_feature_class(engine, id_bd_gdb, version_fc, fc)
-
     # Validator 5 - Quantity of tables
     df_tables = quantity_tables(engine, id_bd_gdb, version_tbl, tbl)
-
     # Validator 6 - Required
     quantity_required(con, id_bd_gdb, version_req, fc)
-
     # Validator 7 - Attributive
     df_attributes = feature_attributes(con, id_bd_gdb, version_att, attributes)
     
